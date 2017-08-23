23e2aad7105be03a32ba17666d36c02922db3352
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-23 11:51:13 +0200

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :polls, only: [:new, :index, :create] do
     resources :targets, only: [:new, :create]
