12a82cdc539d22fb821afbdf49d64d5f3f72d5ac
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-24 15:53:51 +0200

    Optimisation, rajout de la prise en compte de annonyme

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,8 +39,9 @@ photos = ["http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/bijou_2
          "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/voiture_2_720_mqzabf.jpg",
          "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/voiture_1_720_hqe0e2.jpg"]
 9.times do |i|
-  Poll.create!(context: context[i], ends_at: Time.now+7200 , anonym: rand(0..1).to_i, user: User.first, photo_url: photos[i], context_y: context_y.sample)
-  puts "Poll #{i+1} créé"
+  value = [true, false].sample
+  a = Poll.create!(context: context[i], ends_at: Time.now+7200, user: User.first, photo_url: photos[i], context_y: context_y.sample, anonym: value)
+  puts "Poll, anonym: #{a.anonym}, n°#{i+1} créé"
 end
 
 # User.all.each do |user|
