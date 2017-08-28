be8e5b0b5ae1e987eca3c2bee6763f9d65c34dd0
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-28 14:15:05 +0200

    background jobs

--- app/jobs/users_login_job.rb
+++ app/jobs/users_login_job.rb
@@ -3,7 +3,9 @@ class UsersLoginJob < ApplicationJob
 
   def perform(user_id)
     # Do something later
+    puts "Début"
     user = User.find(user_id)
     user.add_groups
+    puts "Fin"
   end
 end
