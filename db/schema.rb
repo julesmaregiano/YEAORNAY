1afcaaa8fd7ce111b1af9cc8974001b31dea823a
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-29 12:27:28 +0200

    add favorite button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828091824) do
+ActiveRecord::Schema.define(version: 20170829084627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20170828091824) do
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_belongings_on_group_id"
     t.index ["user_id"], name: "index_belongings_on_user_id"
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "poll_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_favourites_on_poll_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -107,6 +116,8 @@ ActiveRecord::Schema.define(version: 20170828091824) do
   add_foreign_key "answers", "users"
   add_foreign_key "belongings", "groups"
   add_foreign_key "belongings", "users"
+  add_foreign_key "favourites", "polls"
+  add_foreign_key "favourites", "users"
   add_foreign_key "polls", "users"
   add_foreign_key "targets", "groups"
   add_foreign_key "targets", "polls"
