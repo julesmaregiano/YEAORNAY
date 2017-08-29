46759f838ce1bbb5c75f4e9b22db06f70b998fe7
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-29 12:43:17 +0200

    Favoris ok

--- app/controllers/my/polls_controller.rb
+++ app/controllers/my/polls_controller.rb
@@ -3,6 +3,7 @@ class My::PollsController < ApplicationController
   def index
     @polls = Poll.list(current_user)
     @user = current_user
+    @favourites = current_user.favourites
   end
 
   def show
