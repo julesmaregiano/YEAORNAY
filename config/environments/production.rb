4af46a6ac532c21ca4753819eb2d6e417c4c16ff
jules maregiano <jules.maregiano@gmail.com>
2017-08-31 19:06:16 +0200

    changement du Action Cable pour prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.url = "ws://www.yayornay.io/cable"
-  config.action_cable.allowed_request_origins = [ "http://www.yayornay.io" ]
+  config.action_cable.url = "wss://www.yayornay.io/cable"
+  config.action_cable.allowed_request_origins = [ "http://www.yayornay.io", "https://www.yayornay.io" ]
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
