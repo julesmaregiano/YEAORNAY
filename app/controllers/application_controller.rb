5e28ce3247c29521e18b1afcfe7b2e1a0fa86509
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-31 19:10:39 +0200

    Meta devrais etres bon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
   private
 
   def store_current_location
