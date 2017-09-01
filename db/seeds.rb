9938c44b337a6cd529a7fcd175de13a8f7d88814
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-09-01 12:31:21 +0200

    nouveau seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,23 +32,19 @@ puts 'Captain Anonyme creé'
 puts 'Done.'
 
 puts 'Génération des Polls'
-context = ['Vous en pensez quoi ?', 'Si je met ça à un mariage, yay ou nay?', "Je le prend ? Vite j'ai 10min!! merci!!", "J'hésite à les prendre les filles, yay ou nay?", '85€, je prend?', 'Je rencontre mes beaux-parents demain, YoN ?? :))', 'Pour aller à la plage', 'Moche?', 'Baleze non ?']
+context = ['Nouvelle coupe, bien ?', 'Pour un mariage, yay ou nay?', "Je l'achète ou pas ?", "Pour un apéro avec des amis ?", "Nouvelle raquette, yay ou nay ?"]
 context_y = [50, 75, 25]
 user_id = rand(0..3).to_i
-photos = ["http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/bijou_2_480_tpr3ef.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/bijou_1_360_vyq3el.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/bijou_3_480_yha3yh.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/chaussure_1_480_w3aaqt.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/chaussure_3_zil9u7.jpg",
-         "https://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/chaussure_2_480_ahnnlg.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/voiture_3_480_v6sykx.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/voiture_2_720_mqzabf.jpg",
-         "http://res.cloudinary.com/yay-or-nay/image/upload/v1503328479/voiture_1_720_hqe0e2.jpg"]
+photos = ["http://res.cloudinary.com/zanzibar/image/upload/v1504261382/man_gjr4vv.png",
+         "http://res.cloudinary.com/zanzibar/image/upload/v1504260631/car_rgtboz.jpg",
+         "http://res.cloudinary.com/zanzibar/image/upload/v1504260631/montre_1024_vmazfv.jpg",
+         "http://res.cloudinary.com/zanzibar/image/upload/v1504260631/apero_hcggv3.jpg",
+          "http://res.cloudinary.com/zanzibar/image/upload/v1504260631/tennis_zhs5la.jpg"]
   yon = Group.create(facebook_id: 1696236193750789, name: "Yay or Nay", url: "https://scontent.xx.fbcdn.net/v/t1.0-1/c31.0.50.50/p50x50/20954014_1696236387084103_5962366096731458549_n.png?oh=cc4f425e9f1ba31593391324923c8605&oe=5A2CF534")
   puts yon
-9.times do |i|
+5.times do |i|
   value = [true, false].sample
-  poll = Poll.create(context: context[i], ends_at: Time.now+7200, user: User.first, photo_url: photos[i], context_y: context_y.sample, anonym: true)
+  poll = Poll.create(context: context[i], ends_at: Time.now + 7200, user: User.first, photo_url: photos[i], context_y: context_y.sample, anonym: true)
   target = Target.create(poll_id: poll.id, group_id: yon.id)
   puts "Poll, anonym: #{poll.anonym}, n°#{i+1} créé avec comme target Y"
 end
